76c1b99a2160a7d85699d409ddb134c330e38ca0
zhanglin496 <zhanglin496@163.com>
2016-09-27 18:21:24 +0800

    Update nf_nat.h

--- include/uapi/linux/netfilter/nf_nat.h
+++ include/uapi/linux/netfilter/nf_nat.h
@@ -4,21 +4,24 @@
 #include <linux/netfilter.h>
 #include <linux/netfilter/nf_conntrack_tuple_common.h>
 
-//��ʾ�Ƿ���Ҫ��IP��ַת��
-//������Դ��ַ��Ŀ�ĵ�ַ
+//表示是否需要做IP地址转换
+//可以是源地址和目的地址转换
+//设置了需要做IP地址映射，把IP地址映射到指定的范围内
+//需要检查IP地址是否在配置的范围内
+//如果没有设置该标志，nat模块将不会修改数据包的IP地址
 #define NF_NAT_RANGE_MAP_IPS			(1 << 0)
-//�û��Ƿ�ͨ��iptables ����ʱָ���˶˿ڷ�Χ
-//����MASQURADE ģ���--to-portsѡ��
-//�ں˽��ܵ������ǻ���struct nf_nat_range ���������õĶ˿ڷ�Χ�����ѡ��˿�
+//用户是否通过iptables 配置时指定了端口范围
+//比如MASQURADE 模块的--to-ports选项
+//内核接受到参数是会用struct nf_nat_range 来构造配置的端口范围来随机选择端口
 #define NF_NAT_RANGE_PROTO_SPECIFIED		(1 << 1)
-//������ɶ˿�ƫ��ֵ
+//随机生成端口偏移值，影响端口选择
 #define NF_NAT_RANGE_PROTO_RANDOM		(1 << 2)
-//��ҪӰ��find_best_ips_proto IP��ַѡ��ʱ���㷨
-//�������ڻ�ûŪ���
+//主要影响find_best_ips_proto IP地址选择时的算法，影响端口选择
+//具体现在还没弄清楚
 #define NF_NAT_RANGE_PERSISTENT			(1 << 3)
-//����prandom_u32�������һ��ֵ
+//调用prandom_u32函数随机一个off值，影响端口选择
 #define NF_NAT_RANGE_PROTO_RANDOM_FULLY		(1 << 4)
-//��Ҫ����l4proto->unique_tuple��������ɶ˿�
+//需要调用l4proto->unique_tuple来随机生成端口
 #define NF_NAT_RANGE_PROTO_RANDOM_ALL		\
 	(NF_NAT_RANGE_PROTO_RANDOM | NF_NAT_RANGE_PROTO_RANDOM_FULLY)
 
