4dbc83640ad6686cd4fc0e74c3048e478bb5391a
zhanglin496 <zhanglin496@163.com>
2017-02-24 11:54:04 +0800

    add note

--- net/ipv4/ip_input.c
+++ net/ipv4/ip_input.c
@@ -457,6 +457,7 @@ int ip_rcv(struct sk_buff *skb, struct net_device *dev, struct packet_type *pt, 
 	/* Must drop socket now because of tproxy. */
 	skb_orphan(skb);
 
+	//在hook点执行DNAT后才会调用ip_rcv_finish查找路由
 	return NF_HOOK(NFPROTO_IPV4, NF_INET_PRE_ROUTING, skb, dev, NULL,
 		       ip_rcv_finish);
 
