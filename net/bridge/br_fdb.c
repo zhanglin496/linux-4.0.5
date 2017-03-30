b582c39a1e277a00669e14bd3165ad4591e963ff
zhanglin496 <zhanglin496@163.com>
2017-03-30 21:32:08 +0800

    add bridge note

--- net/bridge/br_fdb.c
+++ net/bridge/br_fdb.c
@@ -575,9 +575,9 @@ void br_fdb_update(struct net_bridge *br, struct net_bridge_port *source,
 		}
 	} else {
 	//未找到fdb数据库，添加新的数据库
-	//一般会有多个端口指向同一个net_devcie
-	//因为每一个net_device可能会收到多个源mac地址不一样的数据包
-	//所以要建立多个net_bridge_fdb_entry数据库条目来记录数据包来自哪个net_device
+	//可能会有多个数据库指向同一个net_bridge_port,但是一个net_bridge_port只会关联到一个固定的net_devcie
+	//因为每一个net_bridge_port可能会收到多个源mac地址不一样的数据包
+	//所以要建立多个net_bridge_fdb_entry数据库条目来记录数据包来自哪个net_bridge_port
 		spin_lock(&br->hash_lock);
 		if (likely(!fdb_find(head, addr, vid))) {
 			fdb = fdb_create(head, source, addr, vid);
